e1c4f9d3e1ee86fc8cc6ba37fc9c7a35b9c6b5f2
Jose Lavado <jose.lavado2016@gmail.com>
2023-01-15 19:10:20 -0500

    Add Shuffle code

---
+++ shuffle.py
@@ -0,0 +1,32 @@
+from random import shuffle
+
+#initial list order
+mylist = [" ","O"," "]
+
+#Shuffle list
+def shuffle_list(list_mod):
+    shuffle(list_mod)
+    return list_mod
+    
+#player chooses where ball is
+def player_guess():
+    guess = "3"
+    while guess not in ["0","1","2"]:
+        guess = input ("Pick a number: 0, 1 or 2: ")
+    return int(guess)
+
+def check_guess(mix_list, index):
+    if mix_list[index] == "O":
+        print ("Correct")
+    else:
+        print ("Wrong guess")
+    return mix_list
+    
+#shuffle list before game
+mixed_list = shuffle_list(mylist)
+#player guesse
+player_pick = player_guess()
+#check guess
+result = check_guess(mixed_list, player_pick)
+print (result)
+
--- tuples/tuples.py
+++ tuples/tuples.py
@@ -1,1 +1,23 @@
-print("hi")
+
+work_hours =[ ("Moe", 1000), ("Mike", 2000), ("Jose", 5000), ("Steve", 500)]
+
+
+def top_employee(employees):
+    current_max = 0
+    top_guy = ""
+
+    for name, hours in employees:
+        if hours > current_max:
+            current_max = hours
+            top_guy = name
+        else:
+            pass
+    return(top_guy, current_max)
+
+
+print (top_employee(work_hours))
+
+
+
+
+
