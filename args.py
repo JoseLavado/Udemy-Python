0b059a58f9d2ea8904fa295f39881cfe001e67c2
Jose Lavado <jose.lavado2016@gmail.com>
2023-01-16 21:58:23 -0500

    add code

--- args.py
+++ args.py
@@ -1,8 +1,11 @@
+#lesson 49
+
 def func(a,b):
     # return 5% of sum
     return sum((a,b))*0.05
 
 def myargs(*args):
+    print(args)
     for n in range(len(args)):
         print(args[n])
 
@@ -13,5 +16,16 @@ value=func(a,b)
 print(f"the sum of {a} and {b} is {a+b} and its 5% is {value}")
  
 myargs(1,3,5,7,9)
+
+
+def pickfruit(**kwargs):
+    print(kwargs)
+    if "fruit" in kwargs:
+        print(f"my fruit is {kwargs['fruit']}")
+
+        
+pickfruit(fruit="apples")
+
+
 
 
