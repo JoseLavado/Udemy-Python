5ffc7c4d6110c15161334217bee7078efec09052
Jose Lavado <jose.lavado2016@gmail.com>
2023-01-17 11:31:01 -0500

    added Section 06 - Exercises file

---
+++ exercises-section06.py
@@ -0,0 +1,26 @@
+def yoda(text):
+    wordlist = text.split()
+    rev_word = wordlist[::-1]
+    return rev_word
+
+def paper_doll(text):
+    new_word=""
+    for i in range(len(text)):
+        new_word = new_word + text[i]*3
+    return new_word
+
+
+rever=yoda("There is no try")
+print(rever)
+final_words=" ".join(rever)
+print(final_words)
+
+print("\n")
+
+print(paper_doll("MILTON"))
+
+
+
+
+
+
