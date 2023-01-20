a98d1c3aa5d26c0a4b39b422d4a915d74343dc99
Jose Lavado <jose.lavado2016@gmail.com>
2023-01-20 13:24:22 -0500

    changes made

--- Summer69.py
+++ Summer69.py
@@ -3,11 +3,9 @@ my_list = [1,2,3,4,5,6,7,8,9,10,11,12]
 def sum69(thelist):
   ADD=True
   Total=0
-
+  
   for i in thelist:
-    if (i==6):
-        ADD=False
-    Total = Total+i
+    print("hello")    
 
 
   return Total
