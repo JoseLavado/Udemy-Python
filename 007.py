6d7410b6c55a52660548fa5b35acb37eabb542df
Jose Lavado <jose.lavado2016@gmail.com>
2023-01-20 21:48:44 -0500

    added teacher example

--- 007.py
+++ 007.py
@@ -20,11 +20,22 @@ def bond_check(arr):
         print(agent)
         return False
 
-my_nums = [1,0,0,17,274,8,5,6,8,3,3,3,3,3,7]
+my_nums = [1,0,0,17,274,8,5,6,8,3,3,3,3,3,8]
 
 result = bond_check(my_nums)
 
 print (result)
+
+print("-----------------")
+
+def spy_game(nums):
+    code = [0,0,7,"x"]
+    for num in nums:
+        if num == code[0]:
+            code.pop(0)
+    return len(code)==1
+
+print(spy_game(my_nums))
 
 
     
