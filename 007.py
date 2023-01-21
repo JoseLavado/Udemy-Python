6839d9db2a665665eec5e3b6884d998dca59f00e
Jose Lavado <jose.lavado2016@gmail.com>
2023-01-20 21:39:44 -0500

    added 007 exercise

--- 007.py
+++ 007.py
@@ -1,35 +1,31 @@
 def bond_check(arr):
     bond = [0,0,7]
-    agent = [1,2,3]
+    agent = [9,9,9]
     arr_len = len(arr)
     x=0
+    print (arr) 
 
     for i in arr:
-        if i == 0 or i == 0 or i == 7:
-            agent[x] == i
+        if i == 0 or i==7:
+            print("i is "+str(i))  
+            agent[x]=i
             x=x+1
-        else:
-            pass
-
+        
     if agent == bond:
+        print(bond)
         print(agent)
         return True
     else:
+        print(bond)
         print(agent)
         return False
 
-my_nums = [1,0,2,4,0,5,6,7]
+my_nums = [1,0,0,17,274,8,5,6,8,3,3,3,3,3,7]
 
 result = bond_check(my_nums)
 
 print (result)
 
-a = [7,2,4]
-b = [7,2,4]
-if a==b:
-    print("a equals b")
-else:
-    print ("a not equals b")
 
     
 
